40e1c1d556f0a45da93cb8e6256d149eed992f7b
irhagram <70169238+irhagram@users.noreply.github.com>
2021-04-03 06:47:05 +0700

    fxmanifest
    untuk memanggil file clientnya ngepotbooster

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+
+game 'gta5'
+title 'Ngepot Booster Made by Roy/anoni4m | github.com/irhagram'
+description 'Ngepot-ngepot nambah nitro tanpa ngerepotin :v'
+author 'anoni4m#6817'
+
+client_scripts {"irhagram-c.lua"}
+
